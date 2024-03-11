7ef0ff5d7c40608cb7af53645db280d7ad3ae1ed
Saif Ur Rehman <saifurrehman.web@gmail.com>
2024-03-11 23:34:12 +0500

    start making

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.scss'
+import Intro from './subHome/intro'
+export default function Home() {
+  return (
+    <div>
+      <Intro/>
+    </div>
+  )
+}
---
+++ src/Pages/Home/subHome/intro.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './intro.scss'
+export default function intro() {
+  return (
+    <div>
+      <h1>Hi from intro</h1>
+    </div>
+  )
+}
